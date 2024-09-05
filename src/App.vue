5e4eafc998a1b0e7054582c1af9070852d61abee
恒 杜 <2020813800@qq.com>
2024-09-05 23:46:20 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+
+<template>
+  <div class="app">
+     hello
+  </div>
+  <Person></Person>
+</template>
+
+<script lang="ts">
+
+import Person from './components/Person.vue';
+
+export default{
+  name: 'App',  //组件名
+  components:{Person}
+}
+</script>
+
+
+<style scoped>
+.app{
+  color: red;
+  background-color: #ddd;
+  box-shadow: 0 0 10px;
+  border-radius: 20px;
+  padding: 30px;
+}
+</style>
---
+++ src/components/Person.vue
@@ -0,0 +1,33 @@
+<!--  -->
+<template>
+  <div class="person">
+    <h2>姓名：{{ name }}</h2>
+    <h2>年龄：{{ age }}</h2>
+    <button @click="showTels">查看联系方式</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+  import {ref } from 'vue';
+
+  let name = ref('张三')
+  let age = ref(18)
+  let tel = '12399999999'
+
+  function changeName() {
+    name.value = 'zhangsan'
+  }
+
+  function showTels(){
+    alert(tel);
+  }
+</script>
+
+<style scoped>
+.person{
+  background-color: rgb(154, 45, 87);
+  box-shadow: 0 0 10px;
+  border-radius: 10px;
+  padding: 20px;
+}
+</style>
